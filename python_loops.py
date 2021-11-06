734771ccc4f6201c5dee00ccf97cbd9f0b94f133
Begaiym Adylbek kyzy <mrmldsky27@gmail.com>
2021-11-06 12:39:31 +0100

    Add solution for task Smycky

--- python_loops.py
+++ python_loops.py
@@ -5,3 +5,40 @@ if user_name in name_list:
 	print('Hello {0}'.format(user_name))
 else:
 	print("Sorry, we don't know such a name. Are you Czech at all?")
+
+# Smycky
+d = {
+	'a': 'alfa',
+	'b': 'bravo',
+	'c': 'charlie',
+	'd': 'delta',
+	'e': 'echo',
+	'f': 'foxtrot',
+	'g': 'golf',
+	'h': 'hotel',
+	'i': 'india',
+	'j': 'juliett',
+	'k': 'kilo',
+	'l': 'lima',
+	'm': 'mike',
+	'n': 'november',
+	'o': 'oscar',
+	'p': 'papa',
+	'q': 'quebec',
+	'r': 'romeo',
+	's': 'sierra',
+	't': 'tango',
+	'u': 'uniform',
+	'v': 'victor',
+	'w': 'whiskey',
+	'x': 'x-ray',
+	'y': 'yankee',
+	'z': 'zulu'
+}
+
+name = input('What is your name? ').lower()
+
+for letter in name:
+	for key, value in d.items():
+		if letter == key:
+			print(value.capitalize())
