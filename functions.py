1bce00c78d0fe47bb8ec65190f2f6c3c850b5699
Begaiym Adylbek kyzy <mrmldsky27@gmail.com>
2021-11-16 14:15:11 +0100

    Add solution for task 1

---
+++ functions.py
@@ -0,0 +1,7 @@
+# 1.1 Prvni cviceni na zaklady funkci
+def divide(dividend, divisor):
+	return dividend // divisor
+
+# 1.2 Druhe cviceni na zaklady funkci
+def sum_nums(num_list):
+	return sum(num_list)
