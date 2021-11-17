4f23e7fd179de3dcda029cf9182f0084cbc47ef7
Begaiym Adylbek kyzy <mrmldsky27@gmail.com>
2021-11-17 11:40:26 +0100

    Fix solutions for 1.task

--- functions.py
+++ functions.py
@@ -1,7 +1,9 @@
 # 1.1 Prvni cviceni na zaklady funkci
 def divide(dividend, divisor):
+	if divisor == 0:
+		return 'You cannot divide by zero'
 	return dividend // divisor
 
 # 1.2 Druhe cviceni na zaklady funkci
 def sum_nums(num_list):
-	return sum(num_list)
+	print('The sum is', sum(num_list))
