331b6fef46ed00f38b78b0af802ad8635d8f0e5e
t-pyrope <kimmarina155@gmail.com>
2021-12-25 12:58:41 +0100

    Move Cat to Animal

--- classes/Cat.py
+++
@@ -1,9 +0,0 @@
-from Animal import Animal
-
-class Cat(Animal):
-	def __init__(self, name):
-		super().__init__(name)
-		self._purr_sound = 'purr purr purr'
-
-	def purr(self):
-		print(self._purr_sound)
